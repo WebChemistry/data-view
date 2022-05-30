05e0eb54f14be372c5bec5918245e38173ec6c81
MartkCz <martk494@gmail.com>
2022-05-30 13:48:49 +0200

    add ArrayDataSource

---
+++ src/DataSource/ArrayDataSource.php
@@ -0,0 +1,64 @@
+<?php declare(strict_types = 1);
+
+namespace WebChemistry\DataView\DataSource;
+
+use WebChemistry\DataView\DataViewComponent;
+
+/**
+ * @template T
+ * @implements DataSource<T>
+ */
+final class ArrayDataSource implements DataSource
+{
+
+	/** @var int<0, max> */
+	private int $offset;
+
+	/**
+	 * @param T[] $array
+	 * @param int<0, max>|null $limit
+	 * @param int<0, max> $offset
+	 */
+	public function __construct(
+		private array $array,
+		private ?int $limit = null,
+		?int $offset = null,
+	)
+	{
+		$this->setOffset($offset);
+	}
+
+	/**
+	 * @param int<0, max>|null $limit
+	 */
+	public function setLimit(?int $limit): void
+	{
+		if ($limit !== null) {
+			$limit = max(0, $limit);
+		}
+
+		$this->limit = $limit;
+	}
+
+	/**
+	 * @param int<0, max>|null $offset
+	 */
+	public function setOffset(?int $offset): void
+	{
+		if ($offset === null) {
+			$offset = 0;
+		}
+
+		$this->offset = max(0, $offset);
+	}
+
+	/**
+	 * @param DataViewComponent<T> $component
+	 * @return DataSet<T>
+	 */
+	public function getDataSet(DataViewComponent $component): DataSet
+	{
+		return new DataSet(count($this->array), array_slice($this->array, $this->offset, $this->limit));
+	}
+
+}
