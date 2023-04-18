8287d524779fb087c3285884d95f77c3613b2a17
MartkCz <martk494@gmail.com>
2023-04-18 14:05:41 +0200

    remove pagination in ArrayDataSource

--- src/DataSource/ArrayDataSource.php
+++ src/DataSource/ArrayDataSource.php
@@ -12,45 +12,13 @@ use WebChemistry\DataView\DataViewComponent;
 final class ArrayDataSource implements DataSource
 {
 
-	/** @var int<0, max> */
-	private int $offset;
-
 	/**
 	 * @param T[] $array
-	 * @param int<0, max>|null $limit
-	 * @param int<0, max>|null $offset
 	 */
 	public function __construct(
 		private array $array,
-		private ?int $limit = null,
-		?int $offset = null,
 	)
 	{
-		$this->setOffset($offset);
-	}
-
-	/**
-	 * @param int<0, max>|null $limit
-	 */
-	public function setLimit(?int $limit): void
-	{
-		if ($limit !== null) {
-			$limit = max(0, $limit);
-		}
-
-		$this->limit = $limit;
-	}
-
-	/**
-	 * @param int<0, max>|null $offset
-	 */
-	public function setOffset(?int $offset): void
-	{
-		if ($offset === null) {
-			$offset = 0;
-		}
-
-		$this->offset = max(0, $offset);
 	}
 
 	/**
@@ -59,7 +27,7 @@ final class ArrayDataSource implements DataSource
 	 */
 	public function getDataSet(DataViewComponent $component): ArrayDataSet
 	{
-		return new ArrayDataSet(count($this->array), array_slice($this->array, $this->offset, $this->limit));
+		return new ArrayDataSet(count($this->array), $this->array);
 	}
 
 }
